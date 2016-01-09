2241ce7961d6e9fc71af5cadad403c27b1b5f584
Sergey Podgornyy <sergey.podgornyy@yahoo.de>
2016-01-09 20:31:34 +0200

    Impove Image Gallery

--- jquery/3_-_Image_gallery/js/app.js
+++ jquery/3_-_Image_gallery/js/app.js
@@ -10,7 +10,9 @@ $overlay.append($image);
 
 var $leftArrow = $("<div id='leftArrow'></div>");
 var $rightArrow = $("<div id='rightArrow'></div>");
+var $closeLightbox = $("<div id='closeLightbox'></div><div style='clear:both'></div>");
 
+$image.before($closeLightbox);
 $image.before($leftArrow);
 $image.after($rightArrow);
 
@@ -23,28 +25,50 @@ $("body").append($overlay);
 //Capture the click event on a link to an image
 $("#imageGallery a").click(function(event){
   event.preventDefault();
-  var imageLocation = $(this).attr("href");
-  //Update overlay with the image linked in the link
-  $image.attr("src", imageLocation);
   
+  getCurrentImage(this);
+
   //Show the overlay.
   $overlay.show();
-  
-  //Get child's alt attribute and set caption
-  var captionText = $(this).children("img").attr("title");
-  $caption.text(captionText);
 });
 
 $leftArrow.click(function(){
-  
+  getPrevImage();
 });
 
 $rightArrow.click(function(){
-  
+  getNextImage();
 });
 
+function getCurrentImage (currentImage) {  
+    thisImage = currentImage;
+    var imageLocation = $(currentImage).attr("href");
+    //Update overlay with the image linked in the link
+    $image.attr("src", imageLocation);
+
+    //Get child's alt attribute and set caption
+    var captionText = $(currentImage).children("img").attr("title");
+    $caption.text(captionText);
+}
+
+function getPrevImage() {
+    imageParent = $(thisImage).parent().prev();
+    thisImage = $(imageParent).children("a");
+    // imageLocation = $(thisImage).attr("href");
+    // $image.attr("src", imageLocation);
+    getCurrentImage(thisImage);
+}
+
+function getNextImage() {
+    imageParent = $(thisImage).parent().next();
+    thisImage = $(imageParent).children("a");
+    // imageLocation = $(thisImage).attr("href");
+    // $image.attr("src", imageLocation);
+    getCurrentImage(thisImage);
+}
+
 //When overlay is clicked
-$overlay.click(function(){
+$closeLightbox.click(function(){
   //Hide the overlay
   $overlay.hide();
 });
