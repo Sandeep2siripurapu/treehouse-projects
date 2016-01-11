6e8fb0f81ab64b7f9f756ffab0d7e5622e0f857c
Sergey Podgornyy <sergey.podgornyy@yahoo.de>
2016-01-11 23:29:15 +0200

    Add interactivity to Star Wars spoilers

--- jquery/2_-_Star_Wars_Spoilers/js/app.js
+++ jquery/2_-_Star_Wars_Spoilers/js/app.js
@@ -1,25 +1,36 @@
 //Prevent spoilerphobes from seeing spoilers
 //Solution: Hide spoilers and reveal them through user interaction
 
-//1, Hide spoiler
+//1. Hide spoiler
 $(".spoiler span").hide();
-//2, Add a button
+//2. Add a button
 $(".spoiler").append("<button>Reveal Spoiler!</button>");
-//3, When button pressed
+//3. When button pressed
 $("button").click(function(){
-  //3.1, Show spoiler next to the button clicked
+  //3.1. Show spoiler next to the button clicked
   $(this).prev().show();
-  //3.2, Get rid of button
+  //3.2. Get rid of button
   $(this).remove();
 });
 
-
+// Show default spoiler
 $(document).ready(function(){
-	$("#spoiler-01").before("<h2>Spoiler to Episode I</h2>");
-	$("#spoiler-02").before("<h2>Spoiler to Episode II</h2>");
-	$("#spoiler-03").before("<h2>Spoiler to Episode III</h2>");
-	$("#spoiler-04").before("<h2>Spoiler to Episode IV</h2>");
-	$("#spoiler-05").before("<h2>Spoiler to Episode V</h2>");
-	$("#spoiler-06").before("<h2>Spoiler to Episode VI</h2>");
-	$("#spoiler-07").before("<h2>Spoiler to Episode VII</h2>");
+	$('#spoiler-01').show().before('<h2>Spoiler to Episode I</h2>');
+});
+
+var $select = $('#spoilers');
+
+//Bind change listener to the select
+$select.change(function(){
+  //Change span text from selected option
+  var $selected = $('#spoilers option:selected');
+
+  $('.form-element span').text($selected.text());
+
+  // Show selected spoiler and hide other spoilers
+  var element = '#' + $('#spoilers').val();
+
+  $('section h2').remove();
+  $('.spoiler').hide();
+  $(element).before('<h2>'+$selected.text()+'</h2>').show();
 });
